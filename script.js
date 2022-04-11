962ef48be89ea0bf13926895e26257e0f58b227a
michele-pavlenco <michelepavlenco.mp@gmail.com>
2022-04-11 15:13:22 +0200

    intestazione

---
+++ script.js
@@ -0,0 +1,114 @@
+const icons =[
+	{
+		name: 'cat',
+		prefix: '<i class="fa-solid fa-cat"></i>',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'crow',
+		prefix: '<i class="fa-solid fa-crow"></i>',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dog',
+		prefix: '<i class="fa-solid fa-dog"></i>',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dove',
+		prefix: '<i class="fa-solid fa-dove"></i>',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dragon',
+		prefix: '<i class="fa-solid fa-dragon"></i>',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'horse',
+		prefix: '<i class="fa-solid fa-horse"></i>',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'hippo',
+		prefix: '<i class="fa-solid fa-hippo"></i>',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'fish',
+		prefix: '<i class="fa-solid fa-fish"></i>',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'carrot',
+		prefix: '<i class="fa-solid fa-carrot"></i>',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'apple-alt',
+		prefix: '<i class="fa-solid fa-apple-whole"></i>',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'lemon',
+		prefix: '<i class="fa-solid fa-lemon"></i>',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'pepper-hot',
+		prefix: '<i class="fa-solid fa-pepper-hot"></i>',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'user-astronaut',
+		prefix: '<i class="fa-solid fa-user-astronaut"></i>',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-graduate',
+		prefix: '<i class="fa-solid fa-user-graduate"></i>',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-ninja',
+		prefix: '<i class="fa-solid fa-user-ninja"></i>',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-secret',
+		prefix: '<i class="fa-solid fa-user-secret"></i>',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	}
+];
